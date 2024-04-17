26bf105325d1057c77a7611680e5238fc2cd947d
serhii-stepanenko <stepanenkoserhii94@gmail.com>
2024-04-17 17:30:54 -0400

    fix: update package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // const axios = require('axios');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 app.use(express.json());
@@ -8,7 +8,7 @@ app.use(express.json());
 const hostURL = 'https://9395342-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=60&deploy=1&compid=9395342_SB1&h=b1f0f120b7294d147886';
 
 // Enable CORS for all routes
-app.use(cors());
+// app.use(cors());
 
 app.get("/", (req, res) => { res.send("Welcome SuiteAccess"); });
 /*
